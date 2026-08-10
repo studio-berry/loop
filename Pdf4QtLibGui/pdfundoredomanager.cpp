8dea731d73b4d8cfe72261d4dc94ae64baf257ff
mbx30 <berryismichael@gmail.com>
2026-08-10 09:02:34 -0700

    Bound and name undo history (#162)

--- Pdf4QtLibGui/pdfundoredomanager.cpp
+++ Pdf4QtLibGui/pdfundoredomanager.cpp
@@ -22,6 +22,9 @@
 
 #include "pdfundoredomanager.h"
 #include "pdfdbgheap.h"
+#include "pdfdocumentwriter.h"
+
+#include <algorithm>
 
 namespace pdfviewer
 {
@@ -52,6 +55,7 @@ void PDFUndoRedoManager::doUndo()
     clampUndoRedoSteps();
 
     Q_EMIT undoRedoStateChanged();
+    Q_EMIT undoHistoryChanged();
     Q_EMIT documentChangeRequest(pdf::PDFModifiedDocument(item.oldDocument, nullptr, item.flags));
 }
 
@@ -70,6 +74,7 @@ void PDFUndoRedoManager::doRedo()
     clampUndoRedoSteps();
 
     Q_EMIT undoRedoStateChanged();
+    Q_EMIT undoHistoryChanged();
     Q_EMIT documentChangeRequest(pdf::PDFModifiedDocument(item.newDocument, nullptr, item.flags));
 }
 
@@ -79,17 +84,57 @@ void PDFUndoRedoManager::clear()
     {
         m_undoSteps.clear();
         m_redoSteps.clear();
+        m_historyMemoryBytes = 0;
+        m_historyTruncated = false;
         Q_EMIT undoRedoStateChanged();
+        Q_EMIT undoHistoryChanged();
     }
 }
 
 void PDFUndoRedoManager::createUndo(pdf::PDFModifiedDocument document, pdf::PDFDocumentPointer oldDocument)
 {
-    m_undoSteps.emplace_back(oldDocument, document, document.getFlags());
+    createUndo(qMove(document), qMove(oldDocument), QString());
+}
+
+void PDFUndoRedoManager::createUndo(pdf::PDFModifiedDocument document,
+                                    pdf::PDFDocumentPointer oldDocument,
+                                    const QString& name)
+{
+    QString resolvedName = name;
+    if (resolvedName.isEmpty())
+    {
+        if (document.hasPageContentsChanged())
+        {
+            resolvedName = QStringLiteral("Edit page content");
+        }
+        else if (document.hasFlag(pdf::PDFModifiedDocument::Annotation))
+        {
+            resolvedName = QStringLiteral("Edit annotation");
+        }
+        else if (document.hasFlag(pdf::PDFModifiedDocument::FormField))
+        {
+            resolvedName = QStringLiteral("Edit form field");
+        }
+        else
+        {
+            resolvedName = QStringLiteral("Document change");
+        }
+    }
+    const size_t estimatedBytes = estimateDocumentBytes(oldDocument.data())
+        + estimateDocumentBytes(document.getDocument());
+    m_undoSteps.emplace_back(oldDocument, document, document.getFlags(),
+                             qMove(resolvedName),
+                             estimatedBytes);
+    m_historyMemoryBytes += estimatedBytes;
+    for (const UndoRedoItem& item : m_redoSteps)
+    {
+        m_historyMemoryBytes -= std::min(m_historyMemoryBytes, item.estimatedBytes);
+    }
     m_redoSteps.clear();
     m_isCurrentSaved = false;
     clampUndoRedoSteps();
     Q_EMIT undoRedoStateChanged();
+    Q_EMIT undoHistoryChanged();
 }
 
 void PDFUndoRedoManager::setMaximumSteps(size_t undoLimit, size_t redoLimit)
@@ -100,21 +145,94 @@ void PDFUndoRedoManager::setMaximumSteps(size_t undoLimit, size_t redoLimit)
         m_redoLimit = redoLimit;
         clampUndoRedoSteps();
         Q_EMIT undoRedoStateChanged();
+        Q_EMIT undoHistoryChanged();
+    }
+}
+
+void PDFUndoRedoManager::setMemoryLimitBytes(size_t memoryLimitBytes)
+{
+    if (m_memoryLimitBytes != memoryLimitBytes)
+    {
+        m_memoryLimitBytes = memoryLimitBytes;
+        clampUndoRedoSteps();
+        Q_EMIT undoRedoStateChanged();
+        Q_EMIT undoHistoryChanged();
     }
 }
 
 void PDFUndoRedoManager::clampUndoRedoSteps()
 {
+    const size_t beforeUndoSize = m_undoSteps.size();
+    const size_t beforeRedoSize = m_redoSteps.size();
     if (m_undoSteps.size() > m_undoLimit)
     {
         // We erase from oldest steps to newest
+        for (auto it = m_undoSteps.begin(); it != std::next(m_undoSteps.begin(), m_undoSteps.size() - m_undoLimit); ++it)
+        {
+            m_historyMemoryBytes -= std::min(m_historyMemoryBytes, it->estimatedBytes);
+        }
         m_undoSteps.erase(m_undoSteps.begin(), std::next(m_undoSteps.begin(), m_undoSteps.size() - m_undoLimit));
     }
     if (m_redoSteps.size() > m_redoLimit)
     {
         // Newest steps are erased
+        for (auto it = std::next(m_redoSteps.begin(), m_redoLimit); it != m_redoSteps.end(); ++it)
+        {
+            m_historyMemoryBytes -= std::min(m_historyMemoryBytes, it->estimatedBytes);
+        }
         m_redoSteps.resize(m_redoLimit);
     }
+
+    while (m_memoryLimitBytes > 0 && m_historyMemoryBytes > m_memoryLimitBytes && !m_undoSteps.empty())
+    {
+        m_historyMemoryBytes -= std::min(m_historyMemoryBytes, m_undoSteps.front().estimatedBytes);
+        m_undoSteps.erase(m_undoSteps.begin());
+    }
+    while (m_memoryLimitBytes > 0 && m_historyMemoryBytes > m_memoryLimitBytes && !m_redoSteps.empty())
+    {
+        m_historyMemoryBytes -= std::min(m_historyMemoryBytes, m_redoSteps.back().estimatedBytes);
+        m_redoSteps.pop_back();
+    }
+
+    if (beforeUndoSize != m_undoSteps.size() || beforeRedoSize != m_redoSteps.size())
+    {
+        markHistoryTruncated();
+    }
+}
+
+size_t PDFUndoRedoManager::estimateDocumentBytes(const pdf::PDFDocument* document)
+{
+    if (!document)
+    {
+        return 0;
+    }
+
+    const qint64 fileSize = pdf::PDFDocumentWriter::getDocumentFileSize(document);
+    if (fileSize <= 0)
+    {
+        return 0;
+    }
+    return static_cast<size_t>(fileSize);
+}
+
+void PDFUndoRedoManager::markHistoryTruncated()
+{
+    if (!m_historyTruncated)
+    {
+        m_historyTruncated = true;
+    }
+    Q_EMIT undoHistoryTruncated();
+}
+
+QStringList PDFUndoRedoManager::getUndoNames() const
+{
+    QStringList names;
+    names.reserve(static_cast<qsizetype>(m_undoSteps.size()));
+    for (const UndoRedoItem& item : m_undoSteps)
+    {
+        names.append(item.name);
+    }
+    return names;
 }
 
 bool PDFUndoRedoManager::isCurrentSaved() const
