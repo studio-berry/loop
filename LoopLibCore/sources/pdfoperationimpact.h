3c585af1f11d2791905fd491394655c8cf291905
michael berry <212453881+mberrys@users.noreply.github.com>
2026-09-18 17:50:46 -0700

    feat(core): model evidence reuse in revalidation plans

--- LoopLibCore/sources/pdfoperationimpact.h
+++ LoopLibCore/sources/pdfoperationimpact.h
@@ -47,6 +47,7 @@ struct LOOPLIBCORESHARED_EXPORT PDFOperationImpact
     bool fullRewrite = false;
     bool impactComplete = false;
     bool requiresIndependentOracle = false;
+    bool mutatesDocument = true;
 
     bool isFullRevalidation() const;
     QJsonObject toJson() const;
@@ -57,6 +58,9 @@ struct LOOPLIBCORESHARED_EXPORT PDFRevalidationPlan
 {
     bool full = true;
     QStringList checkIds;
+    QStringList reusedCheckIds;
+    PDFEvidenceDomains invalidatedEvidenceDomains;
+    PDFEvidenceDomains reusableEvidenceDomains;
     QSet<int> pages;
     QString reason;
 
